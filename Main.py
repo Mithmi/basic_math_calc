b383cc9b5af33e5353fe606b40c65aa225da7983
Mithmi <workaintfun@yahoo.com>
2015-12-21 16:07:32 +0300

    Hello

--- Main.py
+++ Main.py
@@ -1,8 +1,43 @@
+# -*- coding: utf-8 -*-
 import string
+import sys
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
+from PyQt4 import QtCore, QtGui, uic
+form_class = uic.loadUiType("GuiManita.ui")[0]
 x, y, z = symbols('x y z')
 j0, j1, h1, h2 = var('j0 j1 h1 h2')
+
+class MyWindowClass(QtGui.QMainWindow, form_class):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self,parent)
+        self.allowed = "0123456789+-/*xyzcosintgqrab()"
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.Silv_button_clicked)
+        self.pushButton_2.clicked.connect(self.Opti_button_clicked)
+        self.pushButton_3.clicked.connect(self.Lag_button_clicked)
+
+
+    def Silv_button_clicked(self):
+        value = self.lineEdit.text()
+        reforged = filter(value, self.allowed)
+        self.label.setText(reforged)
+        Silv = Silvester(reforged)
+        self.label_2.setText(Silv)
+
+    def Opti_button_clicked(self):
+        value = self.lineEdit_2.text()
+        reforged = filter(value, self.allowed)
+        self.label_3.setText(reforged)
+
+    def Lag_button_clicked(self):
+        value = self.lineEdit_3.text()
+        value1 = self.lineEdit_4.text()
+        reforged = filter(value, self.allowed)
+        reforged1 = filter(value1, self.allowed)
+        self.label_5.setText(reforged)
+        self.label_7.setText(reforged1)
+
 
 
 def fmtstr(fmt, str):
@@ -24,6 +59,7 @@ def filter(inStr, allowed):
     return outStr
 
 def Silvester(func):
+    result = ""
     dx = diff(func, x)
     dy = diff(func, y)
     dz = diff(func, z)
@@ -31,6 +67,7 @@ def Silvester(func):
     que = [x, y, z]
     dque = [dx, dy, dz]
     res = []
+    print(result)
     for i in range(len(que)):
         for j in range(len(que)):
             d = diff(dque[i], que[j])
@@ -40,22 +77,102 @@ def Silvester(func):
     minor1 = M[0, 0]
     minor2 = N.det()
     minor3 = M.det()
+    N = Matrix([res[4:6], res[7:9]])
+    print(N)
+    minor4 = N.det()
+    print(res[0], res[2], res[7], res[8])
+    N = Matrix([[res[0], res[2]], [res[7], res[8]]])
+    print(N)
+    minor5 = N.det()
+    minor6 = res[8]
+    minor7 = res[4]
+    minor8 = res[0]
+    hart = ""
     for k in range(len(result)):
-        m1 = minor1.evalf(subs={x: result[k][x]})
-        m2 = minor2.evalf(subs={x: result[k][x]})
-        m3 = minor3.evalf(subs={x: result[k][x]})
+        try:
+            m1 = minor1.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m2 = minor2.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m3 = minor3.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m4 = minor3.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m5 = minor3.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m6 = minor3.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m7 = minor3.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            m8 = minor3.evalf(subs={x: result[k][x], y: result[k][y], z: result[k][z]})
+            hart += "x = " + str(result[k][x]) + "\n" \
+                + "y = " + str(result[k][y]) + "\n" \
+                + "z = " + str(result[k][z]) + "\n\n"
+        except KeyError:
+            try:
+                m1 = minor1.evalf(subs={x: result[k][x], y: result[k][y]})
+                m2 = minor2.evalf(subs={x: result[k][x], y: result[k][y]})
+                m3 = minor3.evalf(subs={x: result[k][x], y: result[k][y]})
+                m4 = minor3.evalf(subs={x: result[k][x], y: result[k][y]})
+                m5 = minor3.evalf(subs={x: result[k][x], y: result[k][y]})
+                m6 = minor3.evalf(subs={x: result[k][x], y: result[k][y]})
+                m7 = minor3.evalf(subs={x: result[k][x], y: result[k][y]})
+                m8 = minor3.evalf(subs={x: result[k][x], y: result[k][y]})
+                hart += "x = " + str(result[k][x]) + "\n" \
+                + "y = " + str(result[k][y]) + "\n" \
+                + "z = " + "0" + "\n\n"
+            except KeyError:
+                m1 = minor1.evalf(subs={x: result[k][x]})
+                m2 = minor2.evalf(subs={x: result[k][x]})
+                m3 = minor3.evalf(subs={x: result[k][x]})
+                m4 = minor3.evalf(subs={x: result[k][x]})
+                m5 = minor3.evalf(subs={x: result[k][x]})
+                m6 = minor3.evalf(subs={x: result[k][x]})
+                m7 = minor3.evalf(subs={x: result[k][x]})
+                m8 = minor3.evalf(subs={x: result[k][x]})
+                hart += "x = " + str(result[k][x]) + "\n" \
+                    + "y = " + "0" + "\n" \
+                    + "z = " + "0" + "\n\n"
         if m1 >= 0 and m2 >= 0 and m3 >= 0:
-            matrix_status = "matrix >= 0"
+            matrix_status = "Так как все миноры >= 0, следовательно матрица >= 0"
             print(matrix_status)
-            print("X - locmin")
+            end = "А значит X точка - locmin, так как матрица - неотрицательно определена"
         elif m1 <= 0 and m2 >= 0 and m3 <= 0:
-            matrix_status = "matrix <= 0"
+            matrix_status = "Так как некоторые из миноров >= 0, следовательно матрица =< 0"
             print(matrix_status)
-            print("X - locmin")
+            end = "А значит точка X - locmax, так как матрица - неположительно определена"
+        elif m1 > 0 and m2 > 0 and m3 > 0:
+            matrix_status = "Так как все миноры > 0, следовательно матрица > 0"
+            print(matrix_status)
+            end = "А значит точка X - locmin, так как матрица - положительно определена"
+        elif m1 < 0 and m2 < 0 and m3 < 0:
+            matrix_status = "Так как М1 все миноры < 0, следовательно матрица < 0"
+            print(matrix_status)
+            end = "А значит точка X - locmax, так как матрица - отрицательно определена"
         else:
-            matrix_status = "matrix unindentified"
+            matrix_status = "Так как присутствуют Миноры > и < 0, то матрица является знакопеременной"
             print(matrix_status)
-            print("X - isn't locextrm")
+            end = "А значит точка X - не является locextrm"
+    result = "Возьмём производную первого порядка по всем переменным:\n\n" \
+    + "df/dx = " + str(dx) + "\n" \
+    + "df/dy = " + str(dy) + "\n" \
+    + "df/dz = " + str(dz) + "\n\n" \
+    + "Решим уравнение вида df/d = 0:" + "\n\n" \
+    + str(dx) + " =  0" + "\n" \
+    + str(dy) + " =  0" + "\n" \
+    + str(dz) + " =  0" + "\n\n" \
+    + "Отсюда следует что:\n\n" \
+    + hart \
+    + "Построим матрицу производных второго порядка:" + "\n\n" \
+    + str(res[0:3]) + "\n" \
+    + str(res[3:6]) + "\n" \
+    + str(res[6:9]) + "\n\n" \
+    + "Вычисляем угловые миноры:\n\n" \
+    + "M1 = " + str(minor1) + " = " + str(m1) + "\n" \
+    + "M2 = " + str(minor2) + " = " + str(m2) + "\n" \
+    + "M3 = " + str(minor3) + " = " + str(m3) + "\n" \
+    + "M4 = " + str(minor4) + " = " + str(m4) + "\n" \
+    + "M5 = " + str(minor5) + " = " + str(m5) + "\n" \
+    + "M6 = " + str(minor6) + " = " + str(m6) + "\n" \
+    + "M7 = " + str(minor7) + " = " + str(m7) + "\n" \
+    + "M8 = " + str(minor8) + " = " + str(m8) + "\n\n" \
+    + matrix_status + "\n" + end + "\n"
+    print(result)
+    return result
+
 
 def OptimizationDeter(func):
     m = 0
@@ -113,39 +230,18 @@ def Lagrange(func, equation):
     return answer
 
 def main():
-    allowed = "0123456789+-/*xyzcosintgqrab()"
-    task = input('Silv, Opti or Lag?')
-    if task == 'Silv':
-        try:
-            func = str(parse_expr(input('Give me function')))
-            #func = "x**3 + y**2 + 0 * z**2 + y * z * 0 - 3 * x + 6 * y + 2"
-            result = filter(func, allowed)
-            print('func: ', result)
-            Silv = Silvester(result)
-        except (TypeError, ValueError):
-            print('Error')
-    elif task == 'Opti':
-        try:
-            func = str(parse_expr(input('Give me function')))
-            result = filter(func, allowed)
-            #func = "sin(x) * (-x)**5"
-            print('func: ', result)
-            Opti = OptimizationDeter(result)
-        except (TypeError, ValueError):
-            print('Error')
-    elif task == 'Lag':
-        try:
-            func = str(parse_expr(input('Give me function')))
-            equation = str(parse_expr(input('Give me Equation')))
-            result = filter(func, allowed)
-            result1 = filter(equation, allowed)
-            #func = "5 * x**2 + y**2 + 2 * x * y"
-            #equation = "x * y - 10"
-            print('func: ', result, '\n', 'equation: ', result1)
-            Lag = Lagrange(result, result1)
-        except (TypeError, ValueError):
-            print('Error')
+    #func = "x**3 + y**2 + 0 * z**2 + y * z * 0 - 3 * x + 6 * y + 2"
+    #OptiFunc = filter(func, allowed)
+    #func = "sin(x) * (-x)**5"
+    #LagFunc = filter(func, allowed)
+    #LagEqua = filter(equation, allowed)
+    #func = "5 * x**2 + y**2 + 2 * x * y"
+    #equation = "x * y - 10"
+    app = QtGui.QApplication(sys.argv)
+    myWindow = MyWindowClass(None)
+    myWindow.show()
+    app.exec_()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
